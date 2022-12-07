6fa1fcb65d0d8d1974f0ca40ea5ab86bcace7b96
Andrew <cb.project@ya.ru>
2022-12-07 22:30:13 +0500

    fixes: review fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,44 @@
 // @ts-nocheck
-const popupOpenedClose = document.querySelector(".popup__close");
+const popupButtonClose = document.querySelector(".popup__close");
 
-const profile__button_action_edit = document.querySelector(
+const profileEditButton = document.querySelector(
     ".profile__button_action_edit"
 );
-const profile__heading = document.querySelector(".profile__heading");
-const profile__desc = document.querySelector(".profile__desc");
+const profileTitle = document.querySelector(".profile__heading");
+const profileDesc = document.querySelector(".profile__desc");
 const formEditorProfile = document.querySelector(".popup_type_editor-profile");
 const editorProfileForm = document.querySelector(".form_action_edit-profile");
 const inputName = document.querySelector(".form__input_type_name");
 const inputDesc = document.querySelector(".form__input_type_desc");
 
 /**
- *  Обработчики
- */
-profile__button_action_edit.addEventListener("click", openEditorProfile);
-popupOpenedClose.addEventListener("click", closePopup);
-editorProfileForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    profile__heading.textContent = inputName.value
-    profile__desc.textContent = inputDesc.value
-
-    closePopup();
-});
-
-/**
  * Функции
  */
 
 function openEditorProfile() {
-    formEditorProfile.classList.add("popup__opened");
+    formEditorProfile.classList.add("popup_opened");
 
-    inputName.value = profile__heading.textContent
-    inputDesc.value = profile__desc.textContent
+    inputName.value = profileTitle.textContent;
+    inputDesc.value = profileDesc.textContent;
 }
 
 function closePopup() {
-    let popupOpened = document.querySelector(".popup__opened");
-    popupOpened.classList.remove("popup__opened");
+    formEditorProfile.classList.remove("popup_opened");
 }
+
+function submitEditorProfile(e) {
+    e.preventDefault();
+
+    profileTitle.textContent = inputName.value;
+    profileDesc.textContent = inputDesc.value;
+
+    closePopup();
+}
+
+/**
+ *  Обработчики
+ */
+
+profileEditButton.addEventListener("click", openEditorProfile);
+popupButtonClose.addEventListener("click", closePopup);
+editorProfileForm.addEventListener("submit", submitEditorProfile);
