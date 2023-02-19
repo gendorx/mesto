5c75c1fbda983fe133adaf0a138ea7e7b57c8c29
Andrew <cb.project@ya.ru>
2023-02-19 14:10:13 +0500

    fix: added unset listeners submit form

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,18 +6,27 @@ export default class PopupWithForm extends Popup {
 
     this._submitForm = config.submitForm.bind(this);
     this._formElement = this._container.querySelector(config.popupFormSelector);
+
+    this._handlerSubmitForm = this._handleSubmitForm.bind(this);
   }
 
   _getInputValues() {
     return Object.fromEntries(new FormData(this._formElement));
   }
 
+  _handleSubmitForm(evt) {
+    evt.preventDefault();
+    this._submitForm(this._getInputValues());
+  }
+
   setEventListeners() {
     super.setEventListeners();
-    this._formElement.addEventListener("submit", (evt) => {
-      evt.preventDefault();
-      this._submitForm(this._getInputValues());
-    });
+    this._formElement.addEventListener("submit", this._handlerSubmitForm);
+  }
+
+  unsetEventListeners() {
+    super.unsetEventListeners();
+    this._formElement.removeEventListener("submit", this._handlerSubmitForm);
   }
 
   close() {
